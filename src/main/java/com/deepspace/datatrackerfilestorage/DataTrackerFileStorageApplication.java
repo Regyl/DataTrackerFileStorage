a98c9e5af326660a869bed1194a85180d9ef6ec0
Новиков Евгений Алексеевич <eanovikov@phoenixit.ru>
2022-07-17 12:16:55 +0300

    initial commit

---
+++ src/main/java/com/deepspace/datatrackerfilestorage/DataTrackerFileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.deepspace.datatrackerfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTrackerFileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTrackerFileStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepspace/datatrackerfilestorage/DataTrackerFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepspace.datatrackerfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTrackerFileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
